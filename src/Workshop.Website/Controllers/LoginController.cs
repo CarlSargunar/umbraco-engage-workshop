2b683e5faf0ee0f65eefab659c1a46cd33078a04
Jon Whitter <jon.whitter@cantarus.com>
2025-03-25 10:46:28 +0000

    initial commit

---
+++ src/Workshop.Website/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.Security;
+
+namespace Workshop.Website;
+
+public class LoginController : RenderController
+{
+    private readonly MemberManager _memberManager;
+    private readonly MemberSignInManager _memberSignInManager;
+
+    public LoginController(
+        ILogger<RenderController> logger,
+        ICompositeViewEngine compositeViewEngine,
+        IUmbracoContextAccessor umbracoContextAccessor,
+        MemberManager memberManager,
+        MemberSignInManager memberSignInManager) : base(logger, compositeViewEngine, umbracoContextAccessor)
+    {
+        _memberManager = memberManager;
+        _memberSignInManager = memberSignInManager;
+    }
+
+    [NonAction]
+    public sealed override IActionResult Index() => throw new NotImplementedException();
+
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var member = await _memberManager.FindByEmailAsync("workshop@example.com");
+
+        if (member == null)
+        {
+            return NotFound();
+        }
+
+        await _memberSignInManager.SignInAsync(member, false);
+
+        return Redirect("/");
+    }
+}
